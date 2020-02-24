20edbcb74805b1e9a85627a9b46bc085e9d87fc9
matheuscosantos <oliveiramatheus77@gmail.com>
2020-02-24 14:27:44 -0300

    "adicionada a função delete"

--- src/main/java/com/produtos/apirest/resources/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -3,6 +3,7 @@ package com.produtos.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,12 +21,12 @@ public class ProdutoResource {
 	@Autowired
 	ProdutoRepository produtoRepository;
 	
-	@GetMapping("/produtos")
+	@GetMapping("/produto")
 	public List<Produto> listaProdutos(){
 		return produtoRepository.findAll();
 	}
 	
-	@GetMapping("/produtos/{id}")
+	@GetMapping("/produto/{id}")
 	public Produto listaProdutoUnico(@PathVariable(value="id") long id){
 		return produtoRepository.findById(id);
 	}
@@ -34,4 +35,9 @@ public class ProdutoResource {
 	public Produto salvaProduto(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}	
+	
+	@DeleteMapping("/produto")
+	public void deletaProduto(@RequestBody Produto produto) {
+		produtoRepository.delete(produto);
+	}
 }
